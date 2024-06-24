8900e2a66917d49f97d93faf7aa1b82de5f9eba4
dat.truong <dattruong88vn@gmail.com>
2024-06-24 15:40:06 +0700

    GuessWord component

--- src/components/Jotto/GuessWords/GuessWords.tsx
+++ src/components/Jotto/GuessWords/GuessWords.tsx
@@ -1,5 +1,33 @@
-const GuessWords = () => {
-  return <div>GuessWords</div>;
+export interface GuessWordsProps {
+  guessWords: { guessWord: string; letterMatchCount: number }[];
+}
+const GuessWords = ({ guessWords }: GuessWordsProps) => {
+  return (
+    <div data-testId="guess-words">
+      {guessWords.length > 0 ? (
+        <div data-testId="guess-words-list">
+          {guessWords.map((word, index) => (
+            <div
+              key={index}
+              data-testId="guess-word"
+              style={{
+                width: 500,
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <div>{word.guessWord}</div>
+              <div>{word.letterMatchCount}</div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div data-testId="guess-word-instruction">
+          Try to guess secret word!
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default GuessWords;
