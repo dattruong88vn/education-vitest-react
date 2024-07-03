a7f72e458b37dde506f1a1fce620e2a5dab9560f
dat.truong <dattruong88vn@gmail.com>
2024-07-03 09:51:05 +0700

    refactor app with useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer } from "react";
 import Congrats from "./components/Jotto/Congrats/Congrats";
 import GuessWords, {
   GuessWordType,
@@ -7,38 +7,79 @@ import Input from "./components/Jotto/Input/Input";
 import { getLetterMatchCount } from "./helpers";
 import { getSecretWord } from "./actions";
 
+interface State {
+  secretWord: string;
+  guessWords: GuessWordType[];
+  success: boolean;
+}
+
+enum ActionType {
+  SET_SECRET_WORD = "setSecretWord",
+  SET_GUESS_WORDS = "setGuessWords",
+  SET_SUCCESS = "setSuccess",
+}
+
+type Action =
+  | { type: ActionType.SET_SECRET_WORD; payload: string }
+  | { type: ActionType.SET_GUESS_WORDS; payload: GuessWordType[] }
+  | { type: ActionType.SET_SUCCESS; payload: boolean };
+
+const initialState: State = {
+  secretWord: "",
+  guessWords: [],
+  success: false,
+};
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case ActionType.SET_SECRET_WORD:
+      return { ...state, secretWord: action.payload };
+    case ActionType.SET_GUESS_WORDS:
+      return { ...state, guessWords: action.payload };
+    case ActionType.SET_SUCCESS:
+      return { ...state, success: action.payload };
+    default:
+      return state;
+  }
+};
+
 function App() {
-  const [guessWords, setGuessWords] = useState<GuessWordType[]>([]);
-  const [success, setSuccess] = useState(false);
-  const [secretWord, setSecretWord] = useState<string>("party");
+  const [state, dispatch] = useReducer<(state: State, action: Action) => State>(
+    reducer,
+    initialState
+  );
 
   useEffect(() => {
     const getWord = async () => {
       const word = await getSecretWord();
-      setSecretWord(word);
+      dispatch({ type: ActionType.SET_SECRET_WORD, payload: word });
     };
     getWord();
   }, []);
 
   const handleSubmitGuessWord = (value: string) => {
-    if (value === secretWord) {
-      setSuccess(true);
+    if (value === state.secretWord) {
+      dispatch({ type: ActionType.SET_SUCCESS, payload: true });
     }
 
-    const letterMatchCount = getLetterMatchCount(secretWord, value);
+    const letterMatchCount = getLetterMatchCount(state.secretWord, value);
     const data = {
       guessWord: value,
       letterMatchCount,
     };
-    setGuessWords([...guessWords, data]);
+
+    dispatch({
+      type: ActionType.SET_GUESS_WORDS,
+      payload: [...state.guessWords, data],
+    });
   };
 
   return (
     <div data-testid="app" style={{ width: "100%", height: "100%" }}>
       <h1>Jotto Chalenge</h1>
-      <Input success={success} onSubmit={handleSubmitGuessWord} />
-      <Congrats success={success} />
-      <GuessWords guessWords={guessWords} />
+      <Input success={state.success} onSubmit={handleSubmitGuessWord} />
+      <Congrats success={state.success} />
+      <GuessWords guessWords={state.guessWords} />
     </div>
   );
 }
